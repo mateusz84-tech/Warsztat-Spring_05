7b3d12b4d98bb4c0c833e54af4ba0dff34071a44
Mateusz Kocierz <mateusz_kocierz@wp.pl>
2020-04-04 14:45:51 +0200

    dodanie akcji wyswietaljącej listœ ksiażek

--- src/main/java/pl/coderslab/controllers/BookController.java
+++ src/main/java/pl/coderslab/controllers/BookController.java
@@ -1,13 +1,25 @@
 package pl.coderslab.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/books")
 public class BookController {
+
+    // tworzymy atrybut/pole klasy serwisowej
+    private MemoryBookService memoryBookService;
+
+    // tworzymy kontstuktor, przekazujemy jako parametr do konstruktora atrybut klasy serwisowej
+    @Autowired
+    public BookController(MemoryBookService memoryBookService) {
+        this.memoryBookService = memoryBookService;
+    }
 
     @GetMapping("/hello")
     public String hello(){
@@ -20,4 +32,11 @@ public class BookController {
         return new Book(1L,"9788324631766","Thinking in Java",
                 "Bruce Eckel","Helion","programming");
     }
+    // akcja wyświetlająca listę książek
+    @RequestMapping("/showList")
+    public List<Book> showList(){
+
+        return memoryBookService.getList();
+    }
+
 }
