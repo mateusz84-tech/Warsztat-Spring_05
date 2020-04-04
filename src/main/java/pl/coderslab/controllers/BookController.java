bce5cd0a05c412a465f7daf87e9b21ab3445805a
Mateusz Kocierz <mateusz_kocierz@wp.pl>
2020-04-04 15:51:15 +0200

    dodanie akcji dodającej książkę

--- src/main/java/pl/coderslab/controllers/BookController.java
+++ src/main/java/pl/coderslab/controllers/BookController.java
@@ -2,10 +2,7 @@ package pl.coderslab.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -45,6 +42,13 @@ public class BookController {
     public Book showBookById(@PathVariable long id){
 
         return memoryBookService.getBookId(id);
+    }
+
+    // akcja dodająca książkę do listy
+    @PostMapping("/addBook")
+    public void addBook(@RequestBody Book book){
+
+        memoryBookService.addBook(book);
     }
 
 }
