2b1b465ba6c947317ac03a72db55fe36fc2979f8
Mateusz Kocierz <mateusz_kocierz@wp.pl>
2020-04-04 12:56:39 +0200

    initial commit

---
+++ src/main/java/pl/coderslab/AppInitializer.java
@@ -0,0 +1,36 @@
+package pl.coderslab;
+
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.Filter;
+
+
+public class AppInitializer extends
+        AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{AppConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+    // rejestracja filtru ustawiającego odpowiedniego kodowania
+
+    @Override
+    protected Filter[] getServletFilters() {
+        CharacterEncodingFilter characterEncodingFilter =
+                new CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding("UTF-8");
+        return new Filter[]{characterEncodingFilter};
+    }
+}
